c796b851e561c13716b92581e201e436a416f2b4
Sair Ramos <saircontactoramos@gmail.com>
2025-06-05 12:20:48 -0500

    Config vars

--- src/eviroments.js
+++ src/eviroments.js
@@ -36,5 +36,6 @@ export const environment = {
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   NOTIFICACION_MID: "https://pruebasapi.intranetoas.udistrital.edu.co:8527/v1/",
   NOTIFICACION_MID_WS: "wss://pruebasapi.intranetoas.udistrital.edu.co:8527/v1/ws",
-  NOTIFICACIONES_CRUD: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificaciones_crud/"
+  NOTIFICACIONES_CRUD: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificaciones_crud/",
+  MAIN_BACKEND: 'http://observatoriospruebas.portaloas.udistrital.edu.co:4200/api/v1/',
 };
